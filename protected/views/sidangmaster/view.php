f838550c9c254320aa921f7490797bcf2f80ace7
Jajang Nurjaman <Jajang Nurjaman@jajangtea>
2017-07-04 13:45:48 +0700

    input sidang

--- protected/views/sidangmaster/view.php
+++ protected/views/sidangmaster/view.php
@@ -16,14 +16,52 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Sidangmaster #<?php echo $model->IdSidang; ?></h1>
+<div class="row">
+    <div class="col-lg-12">
+        <br/>
+        <div class="main-box clearfix">
+            <header class="main-box-header clearfix">
+                <h2 class="pull-left"><i class="fa fa-bars"></i> Detil Sidang #<?php echo $model->Tanggal; ?></h2> 
+                <div class="filter-block pull-right">                                                   
+                    <a id="ctl0_maincontent_btnPrintOut" class="btn btn-primary pull-left" title="Print Out Daftar Matkul" href="#"><i class="fa fa-print fa-lg"></i></a> 	
+                    <?php echo CHtml::link('<i class="fa  fa-plus-circle fa-lg"></i>', array('create'), array('class' => 'btn btn-primary pull-left')); ?>
+                </div>
+            </header>
+            <div class="main-box-body clearfix">  
+                <?php
+                $this->widget('zii.widgets.CDetailView', array(
+                    'data' => $model,
+                    'attributes' => array(
+                        array(
+                                'name' => 'Tanggal Daftar',
+                                'value' => $model->Tanggal,
+                            ),
+                        array(
+                                'name' => 'Sidang',
+                                'value' => $model->iDJenisSidang->NamaSidang,
+                            ),
+                        array(
+                                'name' => 'TA',
+                                'value' => $model->idTa->Tahun.' - '.$model->idTa->Semester,
+                            ),
+                        array(
+                                'name' => 'status',
+                                'value' => $model->ubahStatus(),
+                            ),
+                        array(
+                                'name' => 'tglBuka',
+                                'value' => $model->tglBuka,
+                            ),
+                        array(
+                                'name' => 'tglTutup',
+                                'value' => $model->tglTutup,
+                            ),
+                        
+                    ),
+                ));
+                ?>
+            </div>
+        </div>
+    </div>
+</div>   
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-	'data'=>$model,
-	'attributes'=>array(
-		'IdSidang',
-		'Tanggal',
-		'IDJenisSidang',
-		'IdTa',
-	),
-)); ?>
